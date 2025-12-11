041235f53d2c3977452881cfcb22a577589b63d3
Pham Hoang Bao Ngoc <nguyettieungoc2002@gmail.com>
2025-12-11 16:55:49 +0700

    update main.ts and data-service.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,14 @@
+import { SinhVien } from "./models/models";
+import { DanhSachSV } from "./services/data-service";
+
 /*
 Used for expanding source code acts as the intermediary in the future
 Base on frameworks structure
 */
+const danhSach = new DanhSachSV();
+const formData = document.querySelector("form");
+const studentTable = document.querySelector("table.student-table");
+
+formData?.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
--- src/services/data-service.ts
+++ src/services/data-service.ts
@@ -8,18 +8,34 @@ export class DanhSachSV {
   }
 
   //Methods
-  private timKiemSV(sv: SinhVien): boolean {
-    return false;
+  private storageFromLocal(sv: SinhVien): void {
+    const data = this.danhSach.map((sv) => {
+      maSV: sv.maSV;
+      tenSV: sv.tenSV;
+      diemToan: sv.diemToan;
+      diemVan: sv.diemVan;
+    });
+
+    localStorage.setItem("danhSachSinhVien", JSON.stringify(data));
+  }
+
+  private timKiemSV(maSV: string): boolean {
+    return this.danhSach.some((sv) => sv.maSV === maSV);
   }
 
   public themSV(sv: SinhVien): void {
-    const tonTai = this.timKiemSV(sv);
+    const tonTai = this.timKiemSV(sv.maSV);
 
     if (tonTai) {
       throw new Error("Student already exists. Add failed!");
     }
-    this.danhSach.push(sv);
+
+    this.danhSach = [...this.danhSach, sv];
   }
 
   public capNhatSV() {}
+
+  public xoaSV(maSV: string) {
+    this.danhSach.filter((sv) => sv.maSV !== maSV);
+  }
 }
